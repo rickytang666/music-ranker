7f26e8601e614c31b5b12bd923429d3d0ce4f882
Ricky Tang <tangricky001@gmail.com>
2026-06-10 21:21:54 -0400

    fix: rails file naming 😫

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -44,7 +44,7 @@ module Backend
     # session middleware required for omniauth oauth state param
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore, key: "_music_ranker_session"
-    require_relative "../app/middleware/omniauth_error_handler"
+    require_relative "../app/middleware/omni_auth_error_handler"
     config.middleware.use OmniAuthErrorHandler
   end
 end
